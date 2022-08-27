526a73dbac61234270ffa12d7a96a2204a9ce47f
Tommy Phan <tommyphan2004.tp@gmail.com>
2022-08-26 22:17:21 -0400

    Little fix.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-    imports: [DatabaseModule, MailModule],
+    imports: [DatabaseModule, MailModule, AuthModule, UserModule, TodoModule],
     controllers: [AppController],
     providers: [AppService],
 })
