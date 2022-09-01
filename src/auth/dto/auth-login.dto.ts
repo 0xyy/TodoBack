2b132ffd60c15061c5e70aa6da1b55d49d8d88b7
Tommy <tommyphan2004.tp@gmail.com>
2022-09-01 22:21:27 +0200

    Added validation in dto.

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,4 +1,14 @@
+import { Contains, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class AuthLoginDto {
+    @IsString()
+    @Contains('@')
+    @MinLength(4)
+    @MaxLength(30)
     email: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(30)
     password: string;
 }
