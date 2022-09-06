0f187d04fa4f3bdc3867bf492b308a64985e6e6a
Tommy <tommyphan2004.tp@gmail.com>
2022-09-06 18:38:46 +0200

    Added cors, global pipe and cookie parser to the app.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-require('dotenv').config()
+import * as cookieParser from 'cookie-parser';
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
+require('dotenv').config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    });
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            disableErrorMessages: true,
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transformOptions: { enableImplicitConversion: true },
+            transform: true,
+            exceptionFactory: (errors: ValidationError[]) => {
+                return new BadRequestException('Validation error.');
+            },
+        }),
+    );
+
+    app.use(cookieParser());
+
     await app.listen(3001);
 }
 bootstrap();
