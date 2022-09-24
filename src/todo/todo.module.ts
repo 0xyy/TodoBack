6de8f427c26afced6556fd79359d5776885b89e7
Tommy <tommyphan2004.tp@gmail.com>
2022-09-24 16:28:29 +0200

    Added import protection with forwardRef.

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
+    imports: [forwardRef(() => UserModule)],
     controllers: [TodoController],
     providers: [TodoService],
 })
-export class TodoModule {
-}
+export class TodoModule {}
