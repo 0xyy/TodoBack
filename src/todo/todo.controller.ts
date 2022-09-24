501fb0a2e27ee42d711b42ff1806e1b1b727eb48
Tommy <tommyphan2004.tp@gmail.com>
2022-09-24 16:29:30 +0200

    Added working add and remove todo functionality.

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { AddTodoDto } from './dto/add-todo.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { UserObj } from '../decorators/user-obj.decorator';
 import { User } from '../user/user.entity';
+import { AddTodoResponse, RemoveTodoResponse } from '../types';
 
 @Controller('todo')
 export class TodoController {
@@ -12,23 +13,36 @@ export class TodoController {
     ) {}
 
     @UseGuards(AuthGuard('jwt'))
-    @Post('/add')
+    @Post('/')
     async addTodo(
         @UserObj() user: User,
         @Body() createTodoDto: AddTodoDto,
-    ) {
+    ): Promise<AddTodoResponse> {
         return this.todoService.add(user, createTodoDto);
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Get('/')
-    async getAllTodo() {
+    async getAllUserTodo(
+        @UserObj() user: User,
+    ) {
         return null;
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Get('/:id')
     async getOneTodo(
         @Param('id') id: string,
     ) {
         return null;
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Delete('/:id')
+    async removeTodo(
+        @UserObj() user: User,
+        @Param('id') id: string,
+    ): Promise<RemoveTodoResponse> {
+        return this.todoService.remove(user, id);
     }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,31 +1,117 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTodoDto } from './dto/create-todo.dto';
+import { Inject, Injectable } from '@nestjs/common';
+import { AddTodoDto } from './dto/add-todo.dto';
 import { Todo } from './todo.entity';
+import { User } from '../user/user.entity';
+import { isBeforeToday } from '../utils/is-before-today';
+import { UserService } from '../user/user.service';
+import { AddTodoResponse, RemoveTodoResponse } from '../types';
 
 @Injectable()
 export class TodoService {
-    async create({ todo, description, isImportant, expiresIn }: CreateTodoDto) {
-        if (!todo.length || todo.length < 30) {
+    constructor(
+       @Inject(UserService) private userService: UserService,
+    ) {}
+
+    async add(user: User, { todo, description, isImportant, expiresIn }: AddTodoDto): Promise<AddTodoResponse> {
+        try {
+            if (!(await this.userService.userExist(user.id))) {
+                return {
+                    isSuccess: false,
+                    message: 'User does not exist.',
+                };
+            }
+
+            if (!todo.length || todo.length > 30) {
+                return {
+                    isSuccess: false,
+                    message: 'Task name must not be empty and less than 30 characters.',
+                };
+            }
+
+            if (description && description.length > 255) {
+                return {
+                    isSuccess: false,
+                    message: 'Description must not be empty and less than 255 characters.',
+                };
+            }
+
+            if (typeof isImportant !== 'boolean') {
+                return {
+                    isSuccess: false,
+                    message: 'Importance of the task must be type of boolean.',
+                };
+            }
+
+            if (isBeforeToday(expiresIn)) {
+                return {
+                    isSuccess: false,
+                    message: 'The expiration date of the task cannot be earlier than the current time.',
+                };
+            }
+
+            const newTodo = new Todo();
+
+            newTodo.todo = todo;
+            newTodo.description = description ? description : null;
+            newTodo.isImportant = isImportant;
+            newTodo.expiresIn = expiresIn;
+            newTodo.user = user;
+
+            await newTodo.save();
+
+            return {
+                isSuccess: true,
+                message: 'Todo has been added successfully.',
+            };
+        } catch (e) {
             return {
                 isSuccess: false,
-                message: '',
-            }
+                message: e.message,
+            };
         }
+    }
 
-        if (description && description.length > 255) {
+    async remove(user: User, todoId: string): Promise<RemoveTodoResponse> {
+        try {
+            if (!(await this.userService.userExist(user.id))) {
+                return {
+                    isSuccess: false,
+                    message: 'User does not exist.',
+                };
+            }
+
+            const todo = await Todo.findOne({
+                where: {
+                    id: todoId,
+                },
+                relations: ['user'],
+            });
+
+            if (!todo) {
+                return {
+                    isSuccess: false,
+                    message: 'You cannot delete todo that does not exist.',
+                };
+            }
+
+            if (user.id !== todo.user.id) {
+                return {
+                    isSuccess: false,
+                    message: 'You cannot delete todo of the other user.',
+                };
+            }
+
+            await todo.remove();
+
+            return {
+                isSuccess: true,
+                message: 'Todo removed successfully.',
+            };
+        } catch (e) {
             return {
                 isSuccess: false,
-                message: '',
-            }
+                message: e.message,
+            };
         }
-
-        const newTodo = new Todo();
-
-        newTodo.todo = todo;
-        newTodo.description = description;
-        newTodo.isImportant = isImportant
-        newTodo.expiresIn = expiresIn;
-
-        await newTodo.save();
     }
 }
